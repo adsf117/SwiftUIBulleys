e8d8dd1134126dbdf812826ab81ef0af5cf57c70
andres.serrano <andres.serrano@globant.com>
2021-01-27 17:49:35 -0500

    Initial Commit

---
+++ Shared/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Shared
+//
+//  Created by Andres David Serrano Vivas on 27/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
