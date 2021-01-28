f21632fca2eedaab8fa4d6949af6d44bb54a6fb9
andres.serrano <andres.serrano@globant.com>
2021-01-28 09:21:47 -0500

    first commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("🎯🎯🎯\nPUT THE BULLEYE AS CLOSE AS YOU CAM TO")
+                .kerning(2.0)
+                .bold()
+                .multilineTextAlignment(.center)
+                .lineSpacing(4.0)
+                .font(.footnote)
+            Text("89")
+                .kerning(-1.0)
+                .fontWeight(.black)
+                .font(.largeTitle)
+            HStack {
+                Text("1")
+                    .bold()
+
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100")
+                    .bold()
+
+            }
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Text("Hit me")
+            })
+        }
     }
 }
 
